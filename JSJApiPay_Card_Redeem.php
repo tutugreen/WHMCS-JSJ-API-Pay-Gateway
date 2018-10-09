acd41d1c5080a69110cc2b64cdfc17f4d073ed73
YuanMing <tutugreen@users.noreply.github.com>
2018-10-10 00:22:58 +0800

    适配新版云锁

---
+++ JSJApiPay_Card_Redeem.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * @WHMCS-JSJ-API-Pay-Gateway
+ *
+ * @For    	   WHMCS 6+
+ * @author     tutugreen (yuanming@tutugreen.com)
+ * @copyright  Copyright (c) Tutugreen.com 2016~2018
+ * @license    MIT
+ * @version    0.20-2018-10-09-01
+ * @link       https://github.com/tutugreen/WHMCS-JSJ-API-Pay-Gateway
+ * 
+ */
+
+require_once("JSJApiPay/JSJApiPay.class.php");
+
+function JSJApiPay_Card_Redeem_config() {
+    $configarray = array(
+		"FriendlyName" => array("Type" => "System", "Value"=>"金莎云[云锁] 卡密兑换 For WHMCS - Code By Tutugreen.com"),
+		"apiid" => array("FriendlyName" => "合作伙伴ID(APIID)", "Type" => "text", "Size" => "25","Description" => "[必填]到你的API后台查找，没有账户的请在 <a href=\"http://api.jsjapp.com/plugin.php?id=add:user&apiid=12744&from=whmcs\" target=\"_blank\" onclick=\"return confirm('此链接为邀请链接，是否同意接口开发者成为阁下的邀请人？')\">这里注册</a> ", ),
+		"apikey" => array("FriendlyName" => "安全检验码(APIKEY)", "Type" => "text", "Size" => "50", "Description" => "[必填]同上",),
+		"buy_link" => array("FriendlyName" => "购卡页面地址", "Type" => "text", "Size" => "50", "Description" => "[必填]引导用户购买卡密。",),
+		"card_amount_1" => array("FriendlyName" => "卡面金额(1/5)", "Type" => "text", "Size" => "50", "Description" => "[必填]准确填写，例：100.00",),
+		"card_amount_2" => array("FriendlyName" => "卡面金额(2/5)", "Type" => "text", "Size" => "50", "Description" => "[必填]准确填写，例：100.00",),
+		"card_amount_3" => array("FriendlyName" => "卡面金额(3/5)", "Type" => "text", "Size" => "50", "Description" => "[必填]准确填写，例：100.00",),
+		"card_amount_4" => array("FriendlyName" => "卡面金额(4/5)", "Type" => "text", "Size" => "50", "Description" => "[必填]准确填写，例：100.00",),
+		"card_amount_5" => array("FriendlyName" => "卡面金额(5/5)", "Type" => "text", "Size" => "50", "Description" => "[必填]准确填写，例：100.00",),
+		"fee_acc" => array("FriendlyName" => "记账手续费[仅显示]", "Type" => "text", "Size" => "50", "Description" => "[必填,不填会报错]默认0，如填写0.01，即是1%手续费，用于WHMCS记账时后台显示和统计，不影响实际支付价格。",),
+		"debug" => array("FriendlyName" => "调试模式", "Type" => "yesno", "Description" => "调试模式,详细LOG请见[WHMCS]/download/JSJApiPay_log.php，使用文件管理或FTP等查看。", ),
+    );
+	return $configarray;
+}
+
+function JSJApiPay_Card_Redeem_link($params) {
+    if (!isset($params['apiid'])) { 
+    echo '$apiid(合作伙伴ID) 为必填项目，请在后台-系统设置-付款-支付接口，Manage Existing Gateways 选项卡中设置。';
+    exit;
+    }
+    if (!isset($params['apikey'])) { 
+    echo '$apikey(安全检验码) 为必填项目，请在后台-系统设置-付款-支付接口设置，Manage Existing Gateways 选项卡中设置。';
+    exit;
+    }
+    if (!isset($params['fee_acc'])) { 
+    echo '$fee_acc(记账手续费) 为必填项目，请在后台-系统设置-付款-支付接口设置，Manage Existing Gateways 选项卡中设置。';
+    exit;
+    }
+    //判断是否需要获取二维码，排除轮询请求节省服务器资源
+    if ($_POST['noqrcode'] or $_GET['noqrcode']){
+        $noqrcode = trim($_POST['noqrcode'] ? $_POST['noqrcode'] : $_GET['noqrcode']);;
+    }else{
+        $noqrcode = "false";
+    }
+    if ($noqrcode == "true") {
+        //轮询请求不需要返回二维码和表单，直接退出
+        return;
+    }
+
+	$JSJApiPay_Card_Redeem_config['input_charset'] = 'utf-8';
+	$JSJApiPay_Card_Redeem_config['apiid'] = trim($params['apiid']);
+	$JSJApiPay_Card_Redeem_config['apikey'] = trim($params['apikey']);
+	$JSJApiPay_Card_Redeem_config['fee_acc'] = trim($params['fee_acc']);
+	$JSJApiPay_Card_Redeem_config['buy_link'] = trim($params['buy_link']);
+	$JSJApiPay_Card_Redeem_config['card_amount_1'] = trim($params['card_amount_1']);
+	$JSJApiPay_Card_Redeem_config['card_amount_2'] = trim($params['card_amount_2']);
+	$JSJApiPay_Card_Redeem_config['card_amount_3'] = trim($params['card_amount_3']);
+	$JSJApiPay_Card_Redeem_config['card_amount_4'] = trim($params['card_amount_4']);
+	$JSJApiPay_Card_Redeem_config['card_amount_5'] = trim($params['card_amount_5']);
+	$debug = trim($params["debug"]);
+
+	#Invoice Variables
+	$amount = $params['amount']; # Format: ##.##
+	$invoiceid = $params['invoiceid']; # Invoice ID Number
+	$description = $params['description']; # Description (eg. Company Name - Invoice #xxx)
+	$amount = $params['amount']; # Format: xxx.xx
+	$currency = $params['currency']; # Currency Code (eg. GBP, USD, etc...
+
+    #System Variables
+	$companyname = $params['companyname'];
+	$system_url = rtrim($params['systemurl'], "/");
+	
+	#Special Variables
+	
+	#如需要指定HTTP/HTTPS可手动修改，参考格式：https://prpr.cloud/modules/gateways/callback/JSJApiPay_callback.php?payment_type=card_redeem&act=return
+	#现已支持HTTPS地址-2016-11-13
+	#$JSJApiPay_Card_Redeem_config['return_url'] = "";
+	$JSJApiPay_Card_Redeem_config['return_url'] = $system_url . "/modules/gateways/callback/JSJApiPay_callback.php";
+
+	/*生成addnum参数:
+	单号格式:wx+您的apiid+字符串
+	*/
+
+	$JSJApiPay_Card_Redeem_config['addnum'] = "wx".$JSJApiPay_Card_Redeem_config['apiid']."001Invoice".$invoiceid."wx";
+
+	//基本参数
+	$parameter = array(
+	"_input_charset"=> trim(strtolower($JSJApiPay_Card_Redeem_config['input_charset'])),
+	"addnum"        => trim($JSJApiPay_Card_Redeem_config['addnum']),
+	"amount"        => number_format(trim($amount),2,".",""),
+	"return_url"	=> trim($JSJApiPay_Card_Redeem_config['return_url'])."&invoiceid=".trim($invoiceid),
+	"invoiceid"		=> trim($invoiceid),
+	"apiid"		    => $JSJApiPay_Card_Redeem_config['apiid'],
+	"apikey"		=> strtolower(md5($JSJApiPay_Card_Redeem_config['apikey'])),
+	"api_url"		=> urlencode(trim($JSJApiPay_Card_Redeem_config['api_url'])),
+	);
+
+	//判断是否已抵达账单页面，兼容手续费插件，减少账单金额更改几率
+	if (!stristr($_SERVER['PHP_SELF'], 'viewinvoice')) {
+		return "处理中";
+	}
+
+	//tooltip提示，判断是否为移动端
+	if (isMobile()) {
+		$tooltip_QRCode_info='<div id="JSJApiPay_Card_Redeem_IMG" data-toggle="tooltip" data-placement="top" title="<h5>欢迎使用微信扫码支付，请使用微信扫一扫支付。</h5>" style="border: 1px solid #AAA;border-radius: 4px;overflow: hidden;padding-top: 5px;">';
+	} else {
+		$tooltip_QRCode_info='<div id="JSJApiPay_Card_Redeem_IMG" data-toggle="tooltip" data-placement="left" title="<h4>欢迎使用微信扫码支付</h4>" style="border: 1px solid #AAA;border-radius: 4px;overflow: hidden;padding-top: 5px;">';
+	}
+
+	$html_code = <<<HTML_CODE
+<!-- Powered By api.jsjapp.com , Coded By Tutugreen.com -->
+<!-- Loading Required JS/CSS -->
+<script type="text/javascript" src="//cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="//cdn.bootcss.com/jquery.qrcode/1.0/jquery.qrcode.min.js"></script>
+<script type="text/javascript" src="//cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<form action="{$JSJApiPay_Card_Redeem_config['return_url']}" method="POST">
+    <input type="hidden" name="payment_type" value="card_redeem">
+    <input type="hidden" name="act" value="redeem">
+    <input type="hidden" name="invoiceid" value="{$parameter['invoiceid']}">
+    <fieldset class="form-group">
+        <label for="card_type">选择卡类型</label>
+        <select class="form-control" id="card_type" name="card_type">
+            <option>请选择</option>
+            <option value="1">{$JSJApiPay_Card_Redeem_config['card_amount_1']} CNY</option>
+            <option value="2">{$JSJApiPay_Card_Redeem_config['card_amount_2']} CNY</option>
+            <option value="3">{$JSJApiPay_Card_Redeem_config['card_amount_3']} CNY</option>
+            <option value="4">{$JSJApiPay_Card_Redeem_config['card_amount_4']} CNY</option>
+            <option value="5">{$JSJApiPay_Card_Redeem_config['card_amount_5']} CNY</option>
+        </select>
+        <small class="text-muted">需与卡面金额一致，结余充入账户余额。</small>
+    </fieldset>
+    <fieldset class="form-group">
+        <label for="card">卡密</label>
+        <input type="password" class="form-control" id="card" name="card" placeholder="例：21A0CCC6D16E99966756A87976E3A197">
+        <small class="text-muted">账单可多次兑换，一次兑换一张。</small>
+    </fieldset>
+    <button type="submit" class="btn btn-primary">核销</button>
+    <a class="btn btn-primary" href="{$JSJApiPay_Card_Redeem_config['buy_link']}" target="_blank">购买卡</a>
+</form>
+<!-- Jquery Polling Invoice & Check Result-->
+<script>
+jQuery(document).ready(function() {
+	var paid_status = false
+	var paid_timer = setInterval(function(){
+		$.ajax({
+			type: "post",
+			url : window.location.href,
+			data : {noqrcode: "true"},
+			dataType : "text",
+			success: function(data){
+				if ( data.indexOf('class="'+"paid"+'"') != -1)
+				{
+					clearInterval(paid_timer)
+					$('#paysuccess').modal('show')
+					setTimeout(function(){location.reload()},3000)
+				}
+			}})
+	},1500)
+})
+</script>
+<div class="modal fade" id="paysuccess">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title"><p class="text-success">支付成功</p></h4>
+			</div>
+			<div class="modal-body">
+				<p>本页面将在3秒后刷新</p>
+			</div>
+		</div>
+	</div>
+</div>
+HTML_CODE;
+
+	if ($debug) {
+		$msg="[YM01ApiPay_card_redeem]订单: $invoiceid 生成支付表单 $html_code";
+		JSJApiPay_logResult($msg);
+	}
+ 	return $html_code;
+
+}
+?>
--- callback/JSJApiPay_callback.php
+++ callback/JSJApiPay_callback.php
@@ -35,6 +35,8 @@ if ($_POST['payment_type'] or $_GET['payment_type']){
     	$gatewaymodule = "JSJApiPay_WeChat_Pay_QRCode";
     } elseif ($incoming_payment_type == 'qq_pay_qrcode'){
     	$gatewaymodule = "JSJApiPay_QQ_Pay_QRCode";
+    } elseif ($incoming_payment_type == 'card_redeem'){
+    	$gatewaymodule = "JSJApiPay_Card_Redeem";
     } else {
     	if ($debug) {
     		$msg="[JSJApiPay]收到未知回调，payment_type 变量缺失或错误";
@@ -42,6 +44,7 @@ if ($_POST['payment_type'] or $_GET['payment_type']){
     	}
     	$api_pay_failed = "true";
     	echo "非法访问";
+    	exit;
     }
 }else{
 	if ($debug) {
@@ -50,6 +53,7 @@ if ($_POST['payment_type'] or $_GET['payment_type']){
 	}
 	$api_pay_failed = "true";
 	echo "非法访问";
+	exit;
 }
 
 if ($_POST['act'] or $_GET['act']){
@@ -85,6 +89,131 @@ if ($api_pay_failed<>"true"){
 		$JSJApiPay_config['apikey'] = trim($gateway['apikey']);
 		$JSJApiPay_config['fee_acc'] = trim($gateway['fee_acc']);
 
+	if ($act=='redeem' and $incoming_payment_type == 'card_redeem'){
+        $incoming_invoiceid = $_POST['invoiceid'] ? $_POST['invoiceid'] : $_GET['invoiceid'];//账单ID
+        if(!ctype_digit($incoming_invoiceid)){
+        	if ($debug) {
+        		$msg="[JSJApiPay]收到未知回调，invoiceid 变量缺失或错误";
+        		JSJApiPay_logResult($msg);
+        	}
+        	$api_pay_failed = "true";
+        	echo "核验失败。如需帮助，请联系客户支持。";
+        	exit;
+        }
+        $invoiceid = $incoming_invoiceid;
+		$card = $_POST['card'] ? $_POST['card'] : $_GET['card'];//卡密
+        if(!ctype_alnum($card) or strlen($card) > 32){
+        	if ($debug) {
+        		$msg="[JSJApiPay]收到未知回调，card 变量缺失或错误";
+        		JSJApiPay_logResult($msg);
+        	}
+        	$api_pay_failed = "true";
+        	echo "核验失败。如需帮助，请联系客户支持。";
+        	exit;
+        }
+		$transid    = "JSJApiPay_Card_".$card;
+		$card_type = $_POST['card_type'] ? $_POST['card_type'] : $_GET['card_type'];//卡面金额
+		if($card_type=="1"){//从
+		    $amount=number_format(trim($gateway['card_amount_1']),2,".","");
+		}elseif($card_type=="2"){
+		    $amount=number_format(trim($gateway['card_amount_2']),2,".","");
+		}elseif($card_type=="3"){
+		    $amount=number_format(trim($gateway['card_amount_3']),2,".","");
+		}elseif($card_type=="4"){
+		    $amount=number_format(trim($gateway['card_amount_4']),2,".","");
+		}elseif($card_type=="5"){
+		    $amount=number_format(trim($gateway['card_amount_5']),2,".","");
+		}else{
+        	if ($debug) {
+        		$msg="[JSJApiPay]收到未知回调，amount 变量缺失或错误";
+        		JSJApiPay_logResult($msg);
+        	}
+        	$api_pay_failed = "true";
+        	echo "核验失败。如需帮助，请联系客户支持。";
+        	exit;
+		}
+
+    	if(!get_magic_quotes_gpc()){
+    	    $card = addslashes($card);
+    	}
+
+    	$tid = $_GET['tid'];
+    	//平台规则生成查询数据
+    	$query_key = md5($JSJApiPay_config['apikey'].$card);
+    	$curl_query_card_status_postfields = array('kami' => $card, 'apiid' => $JSJApiPay_config['apiid'], 'total' => $amount, 'chakey' => $query_key);
+    
+    	//创建CURL，向API核销卡密
+    	$curl_query_card_status = curl_init();
+    	curl_setopt($curl_query_card_status, CURLOPT_URL, "https://yun.jsjapp.com/k/q_k.php");
+    	curl_setopt($curl_query_card_status, CURLOPT_POST, 1);
+    	curl_setopt($curl_query_card_status, CURLOPT_TIMEOUT, 3);
+    	curl_setopt($curl_query_card_status, CURLOPT_FRESH_CONNECT, 1);
+    	curl_setopt($curl_query_card_status, CURLOPT_SSL_VERIFYPEER, true);
+    	curl_setopt($curl_query_card_status, CURLOPT_SSL_VERIFYHOST, true);
+    	curl_setopt($curl_query_card_status, CURLOPT_HEADER, 0);
+    	curl_setopt($curl_query_card_status, CURLOPT_RETURNTRANSFER, 1);
+    	curl_setopt($curl_query_card_status, CURLOPT_POSTFIELDS, $curl_query_card_status_postfields);
+    	curl_setopt($curl_query_card_status, CURLOPT_USERAGENT, "WHMCS_PHP_CURL");
+    	//存储字符
+    	$curl_query_card_status_data = trim(trim(curl_exec($curl_query_card_status)), "\xEF\xBB\xBF");
+    	//关闭CURL
+    	curl_close($curl_query_card_status);
+    
+    	$curl_query_card_status_data = trim($curl_query_card_status_data, "\xEF\xBB\xBF");
+    	if($curl_query_card_status_data=="9"){			//卡密已被使用过
+        	echo "销卡失败，卡密已被使用过，请检查账单状态。如需帮助，请联系客户支持。";
+        	exit;
+    	}else if($curl_query_card_status_data=="4"){		//卡密无效
+        	echo "核验失败。如需帮助，请联系客户支持。";
+        	//var_dump($curl_query_card_status_postfields);//调试
+        	exit;
+    	}else if($curl_query_card_status_data=="1"){		//判断正确，可以使用的卡密，首次销卡。
+			//正确的路径，合法的参数，的确支付过的会员
+			/********************************************
+				这里根据业务逻辑编写相应的程序代码。
+				1、（本条由WHMCS处理）checkCbInvoiceID 会确认交易流水号的唯一性，防止刷单，如存在将exit自动停止。
+				2、（本条由WHMCS处理）addInvoicePayment 会校验交易基本信息，包括金额，完成自动入账，失败将自动exit退出。
+			********************************************/
+            # convert the currency where necessary
+            $userCurrency = getCurrency(Capsule::table("tblinvoices")->where("id",$invoiceid)->get()[0]->userid);
+            $userCurrency_id = $userCurrency["id"];
+            $userCurrency_suffix = $userCurrency["suffix"];
+            if($gateway['convertto'] && ($userCurrency != $gateway['convertto'])) {
+                # the users currency is not the same as the JSJApiPay currency, convert to the users currency
+                $amount = convertCurrency($amount,$gateway['convertto'],$userCurrency_id);
+                $fee   = convertCurrency($fee,$gateway['convertto'],$userCurrency_id);
+            }
+			if ($debug) JSJApiPay_logResult("[JSJApiPay]订单 $invoiceid 兑换验证成功，如入账成功详细参数可在WHMCS-财务记录-接口日志(网关事务日志)中查看");
+        	$invoiceid = checkCbInvoiceID($invoiceid,$gateway["name"]); # Checks invoice ID is a valid invoice number or ends processing
+        	checkCbTransID($transid);
+        	addInvoicePayment($invoiceid,$transid,$amount,$fee,$gatewaymodule);
+        	logTransaction($gateway["name"],$_POST,"Successful-A");
+			//注意，如果你的WHMCS目录比较特殊或需要修改目的地，请在这里修改回调目的地，改为你的账单页面或其他。
+			////header("location:../../../viewinvoice.php?id=$invoiceid&from=paygateway&status=redeemsuccess");
+			$html_code = <<<HTML_CODE
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+	<title>核验结果</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head> 
+<body style=" font-family: Microsoft YaHei;">
+<script type="text/javascript">
+alert("卡密核销成功，感谢您的付款，金额已添加到账单，请关注交易流水。\\n\\n提示：如卡面金额有结余，将自动冲入账户余额。");
+window.location="../../../viewinvoice.php?id={$invoiceid}&from=paygateway&status=redeemsuccess";
+
+</script>
+</body>
+</html>
+HTML_CODE;
+            echo $html_code;
+        	exit;
+    	}else{
+        	echo "核验失败。如需帮助，请联系客户支持。";
+        	exit;
+    	}
+	}
 	if ($act=='return' or $act=='bd'){
 		//回调地址
 		/********************************************
@@ -192,7 +321,7 @@ if ($api_pay_failed<>"true"){
         	checkCbTransID($transid);
         	addInvoicePayment($invoiceid,$transid,$amount,$fee,$gatewaymodule);
         	logTransaction($gateway["name"],$_POST,"Successful-A");
-			exit;
+        	exit;
 		}
 	}
 }
